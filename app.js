bbf6f959254f71512779cd62bd454d5e24965161
zouyifeng <zouyifeng@aliyun.com>
2018-09-28 15:04:41 +0800

    cors设置
    cors设置
    
    cors设置
    
    cors设置

--- app.js
+++ app.js
@@ -30,8 +30,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header("Access-Control-Allow-Origin", "http://localhost:8888");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin");
   next();
 });
 
