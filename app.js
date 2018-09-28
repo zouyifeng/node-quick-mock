4f69ab84b82597fd6897c626ace2f502cbbf390d
zouyifeng <zouyifeng@aliyun.com>
2018-09-28 15:08:14 +0800

    cors设置

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json())
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header("Access-Control-Allow-Origin", "http://localhost:8888");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, access-control-allow-origin");
   next();
 });
 
